399f6ad3f7c6a2434ede34b0caf4ef94aa85907e
David Huancero <info@dhuanco.com>
2024-10-08 23:57:15 +0200

    Init Curriculum Vitae

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(() => {
+  $("#selectMenu").change(function (e) {
+    e.preventDefault();
+
+    console.log();
+    console.log(e.target.value);
+
+    switch ($(this).val()) {
+      case "primero":
+        window.location.href = "https://google.es";
+        break;
+      case "segundo":
+        window.location.href = "#pFinal";
+        break;
+
+      case "tercero":
+        window.location.href = "https://elpais.es";
+        break;
+      default:
+        break;
+    }
+  });
+});
