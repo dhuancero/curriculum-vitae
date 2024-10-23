2a98201e306b89f2e2946166addb5d7de7f26815
David Huancero <info@dhuanco.com>
2024-10-23 23:59:48 +0200

    Change from index.html

--- js/main.js
+++ js/main.js
@@ -9,84 +9,74 @@ const formation = document.querySelector(".formacion");
 //-------------------------------------------------
 
 /* CREO UNA FUNCIÓN PARA LOS DATOS DE LA FORMACIÓN */
-crear_formacionComplementario(formacion_complementaria);
 crear_formacion(formacion);
-
-function crear_formacionComplementario(data) {
-  const divFormacion = document.createElement("div");
-  divFormacion.classList.add("formacion", "gap-3", "border", "border-1");
-  data.forEach((elemento) => {
-    divFormacion.appendChild(cardFormationComplementaria(elemento));
-  });
-  formation.appendChild(divFormacion);
-}
-
-/* CREAR EL CARD FORMACION COMPLEMENTARIA */
-function cardFormationComplementaria(data) {
-  const card = document.createElement("div");
-  card.classList.add("card-body", "card", "bg-light", "shadow-sm");
-  card.innerHTML = `
-      <h5 class="card-title">${data.nombre_formacion}</h5>
-      <p class="card-text">
-        <strong>Centro de formación:</strong> ${data.centro_formacion}
-      </p>
-       <p class="card-text">
-        <strong>Código:</strong> ${data.cod_formacion}
-      </p>
-      <p class="card-text">
-        <strong>Fecha de inicio:</strong> ${data.fecha_inicio}
-      </p>
-      <p class="card-text">
-        <strong>Fecha de finalización:</strong> ${data.fecha_fin}
-      </p>
-      <p class="card-text">
-        <strong>Ciudad:</strong> ${data.ciudad}
-      </p>
-      <p class="card-text">
-        <strong>Modalidad:</strong> ${data.modalidad}
-      </p>
-      <p class="card-text">
-        <strong>Tipo de formación:</strong> ${data.tipo_formacion}
-      </p>
-`;
-  return card;
-}
+crear_formacionComplementario(formacion_complementaria);
 
 /* CREAR EL CARD FORMACION */
 function crear_formacion(data) {
   const titleFormacion = document.createElement("h2");
-  titleFormacion.innerHTML = "Formación ";
-  const divFormacion = document.createElement("div");
-  divFormacion.classList.add("card-body", "card", "bg-light", "shadow-sm");
+  titleFormacion.innerHTML = "Formación";
+  const divFormacion = document.createElement("ul");
+  divFormacion.classList.add("bg-light", "shadow-sm");
   data.forEach((elemento) => {
     divFormacion.innerHTML += `
-      <h5 class="card-title">${elemento.nombre_formacion}</h5>
-      <p class="card-text">
-        <strong>Centro de formación:</strong> ${elemento.centro_formacion}
-      </p>
-      <p class="card-text">
-        <strong>Fecha de inicio:</strong> ${elemento.fecha_inicio}
-      </p>
-      <p class="card-text">
-        <strong>Fecha de finalización:</strong> ${elemento.fecha_fin}
-      </p>
-      <p class="card-text">
-        <strong>Ciudad:</strong> ${elemento.ciudad}
-      </p>
-      <p class="card-text">
-        <strong>Modalidad:</strong> ${elemento.modalidad}
-      </p>
-      <p class="card-text">
-        <strong>Tipo de formación:</strong> ${elemento.tipo_formacion}
-      </p>
+     <li>
+      <h5 class="card-title"><strong>${elemento.nombre_formacion}</strong></h5>
+      <strong>Centro de formación:</strong> ${elemento.centro_formacion}<br/>
+      <strong>Fecha de inicio:</strong> ${elemento.fecha_inicio}<br/>
+      <strong>Fecha de finalización:</strong> ${elemento.fecha_fin}<br/>
+      <strong>Ciudad:</strong> ${elemento.ciudad}<br/>
+      <strong>Modalidad:</strong> ${elemento.modalidad}<br/>
+      <strong>Tipo de formación:</strong> ${elemento.tipo_formacion}<br/>
+      <br/><br/>
+     </li> 
 `;
   });
   formation.appendChild(titleFormacion);
   formation.appendChild(divFormacion);
 }
 
-function cardFormation(data) {
+/** FORMACION COMPLEMENTARIA **/
+
+function crear_formacionComplementario(data) {
+  const titleFormacion = document.createElement("h2");
+  titleFormacion.innerHTML = "Formación complementaria";
+  const divFormacion = document.createElement("ul");
+  divFormacion.classList.add("formacion", "bg-light", "shadow-sm");
+  data.forEach((data) => {
+    divFormacion.innerHTML += `
+    <li>
+    <h5 class="card-title"><strong>${data.nombre_formacion}</strong></h5>
+    <strong>Centro de formación:</strong> ${data.centro_formacion}      <br/>
+    <strong>Código:</strong> ${data.cod_formacion}<br/>
+    <strong>Fecha de inicio:</strong> ${data.fecha_inicio}<br/>
+    <strong>Fecha de finalización:</strong> ${data.fecha_fin}<br/>
+    <strong>Ciudad:</strong> ${data.ciudad} <br/>
+    <strong>Modalidad:</strong> ${data.modalidad} <br/>      
+    <strong>Tipo de formación:</strong> ${data.tipo_formacion}
+    <br/><br/>    
+    </li>
+`;
+  });
+  formation.appendChild(titleFormacion);
+  formation.appendChild(divFormacion);
+}
+
+function cardFormationComplementaria(data) {
+  /* CREAR EL CARD FORMACION COMPLEMENTARIA */
   const card = document.createElement("div");
+  card.classList.add("bg-light", "shadow-sm");
+  card.innerHTML = `
+      <h5 class="card-title">${data.nombre_formacion}</h5>
+      <strong>Centro de formación:</strong> ${data.centro_formacion}      <br/>
+      <strong>Código:</strong> ${data.cod_formacion}<br/>
+      <strong>Fecha de inicio:</strong> ${data.fecha_inicio}<br/>
+      <strong>Fecha de finalización:</strong> ${data.fecha_fin}<br/>
+      <strong>Ciudad:</strong> ${data.ciudad} <br/>
+      <strong>Modalidad:</strong> ${data.modalidad} <br/>      
+      <strong>Tipo de formación:</strong> ${data.tipo_formacion}
+      <br/><br/>    
+`;
   return card;
 }
 
@@ -95,11 +85,28 @@ function cardFormation(data) {
  *   */
 
 crear_habilidades(habilidades);
+
 function crear_habilidades(data) {
-  const divHabilidades = document.createElement("p");
-  divHabilidades.classList.add("habilidades");
-  data.forEach((elemento) => {
-    divHabilidades.innerHTML += `<strong class="">${elemento}</strong> <br>`;
-  });
+  console.log(data);
+  const mitad = data.length / 2;
+  console.log(mitad);
+  const divHabilidades = document.createElement("div");
+  divHabilidades.classList.add("orderHabilidades");
+
+  const ulLeft = document.createElement("ul");
+  const ulRight = document.createElement("ul");
+
+  for (let i = 0; i < data.length; i++) {
+    if (i < mitad) {
+      ulLeft.innerHTML += `<li><strong class="">${data[i]}</strong></li>`;
+    }
+  }
+  for (let i = 0; i < data.length; i++) {
+    if (i > mitad) {
+      ulRight.innerHTML += `<li><strong class="">${data[i]}</strong></li>`;
+    }
+  }
+  divHabilidades.appendChild(ulLeft);
+  divHabilidades.appendChild(ulRight);
   skills.appendChild(divHabilidades);
 }
